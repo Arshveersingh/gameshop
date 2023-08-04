4a69e193b625366a802c4a6c466fb77b8721f008
Arshveer Singh <arshveer439@gmail.com>
2023-08-04 15:02:50 -0300

    Refactor: move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import { PlatfromIconList } from "./PlatfromIconList";
 import { CriticScore } from "./CriticScore";
 import getCroppedImages from "../services/image-url";
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,13 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genres";
 import APIClient, { FetchResponse } from "../services/api-client";
-export interface Genre {
-  id: number;
-  name: string;
-  slug: string;
-  games_count: number;
-  image_background: string;
-}
+import { Genre } from "../entities/Genre";
 const apiClient = new APIClient<Genre>("/genres");
 
 const useGenres = () => {
