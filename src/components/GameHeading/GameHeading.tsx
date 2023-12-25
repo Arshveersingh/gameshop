0704944c70cd7c465a46d583f094cb4700cc1ae4
kamalpreetkaur1621 <arshveer439@gmail.com>
2023-12-25 06:07:23 +0530

    Change home page heading base on search

--- src/components/GameHeading/GameHeading.tsx
+++ src/components/GameHeading/GameHeading.tsx
@@ -4,14 +4,25 @@ import usePlaform from "../../hooks/usePlatform";
 import useGameQueryStore from "../../stores/GameQueryStore";
 import styles from "./GameHeadsing.module.css";
 
+const convertToTitleCase = (str: string) => {
+  if (!str) return "";
+  return str
+    .toLowerCase()
+    .split(/\s+/)
+    .map((word) => word.charAt(0).toUpperCase().concat(word.substring(1)))
+    .join(" ");
+};
 export const GameHeading = () => {
   const genreId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const searchText = useGameQueryStore((s) => s.gameQuery.searchText);
   const genre = useGenre(genreId);
 
   const platformId = useGameQueryStore((s) => s.gameQuery.platformId);
   const platform = usePlaform(platformId);
 
-  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
+  const heading = searchText
+    ? `Search: ${convertToTitleCase(searchText)}`
+    : `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
     <Heading
       className={styles.gameHeading}
