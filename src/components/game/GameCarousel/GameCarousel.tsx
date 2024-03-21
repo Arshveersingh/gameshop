edabd6ff2791525ffecfcb97b89381cceb6aac94
Arshveer Singh <arshveer439@gmail.com>
2024-03-21 13:45:57 +0530

    Add skeleton for swiper component.

--- src/components/game/GameCarousel/GameCarousel.tsx
+++ src/components/game/GameCarousel/GameCarousel.tsx
@@ -1,14 +1,14 @@
-import { Box, Button, Image } from "@chakra-ui/react";
+import { Box, Button, Image, Skeleton } from "@chakra-ui/react";
 import { useState } from "react";
 import { FaRegCirclePlay } from "react-icons/fa6";
-import { FreeMode, Keyboard, Navigation, Thumbs } from "swiper/modules";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import {
-  IoIosArrowDropright,
-  IoIosArrowDropleft,
   IoIosArrowBack,
+  IoIosArrowDropleft,
+  IoIosArrowDropright,
   IoIosArrowForward,
 } from "react-icons/io";
+import { FreeMode, Keyboard, Navigation, Thumbs } from "swiper/modules";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import useScreenshots from "../../../hooks/useScreenshots";
 import useTrailers from "../../../hooks/useTrailers";
 import getCroppedImages from "../../../services/image-url";
@@ -28,108 +28,82 @@ interface Props {
 export const GameCarousel = ({ gameId }: Props) => {
   const swiper = useSwiper();
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   const [swiperActiveIndex, setSwiperActiveIndex] = useState(0);
   const { data: trailers } = useTrailers(gameId);
   const { data: screenshots, isLoading, error } = useScreenshots(gameId);
 
-  if (isLoading) return null;
+  if (isLoading)
+    return (
+      <>
+        <Skeleton
+          className={styles.slide}
+          height={"35vw"}
+          borderRadius={"15px"}
+        ></Skeleton>
+        <Box
+          display={"flex"}
+          margin={"auto"}
+          width="50vw"
+          height={"6vw"}
+          justifyContent={"space-between"}
+        >
+          <Skeleton
+            borderRadius={"15px"}
+            className={styles.thumbnail}
+            paddingX={4}
+            aspectRatio={"16 / 9"}
+          ></Skeleton>
+          <Skeleton
+            borderRadius={"15px"}
+            className={styles.thumbnail}
+            aspectRatio={"16 / 9"}
+          ></Skeleton>
+          <Skeleton
+            borderRadius={"15px"}
+            className={styles.thumbnail}
+            aspectRatio={"16 / 9"}
+          ></Skeleton>
+          <Skeleton
+            borderRadius={"15px"}
+            className={styles.thumbnail}
+            aspectRatio={"16 / 9"}
+          ></Skeleton>
+        </Box>
+      </>
+    );
   if (error) throw error;
 
   return (
-    <Box marginBottom={5} position={"relative"}>
-      <Swiper
-        onSlideChange={(swiper) => {
-          let videoElements = document.getElementsByTagName("video");
-          for (let i = 0; i < videoElements.length; i++) {
-            videoElements[i].pause();
-          }
-          setSwiperActiveIndex(swiper.activeIndex);
-        }}
-        loop={false}
-        spaceBetween={50}
-        navigation={{
-          nextEl: ".nextButton",
-          prevEl: ".prevButton",
-        }}
-        keyboard={true}
-        slidesPerView={1}
-        modules={[Thumbs, FreeMode, Navigation, Keyboard]}
-        thumbs={{
-          //@ts-ignoreF
-          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-        }}
-      >
-        <Button
-          className="prevButton"
-          left={"12%"}
-          onClick={() => swiper.slidePrev()}
-          padding={0}
-          position={"absolute"}
-          top={"50%"}
-          transform={"translateY(-50%);"}
-          variant={"none"}
-          zIndex={100}
-        >
-          <IoIosArrowDropleft size={"3rem"}></IoIosArrowDropleft>
-        </Button>
-        <Button
-          className="nextButton"
-          onClick={() => swiper.slideNext()}
-          padding={0}
-          position={"absolute"}
-          right={"12%"}
-          top={"50%"}
-          transform={"translateY(-50%);"}
-          variant={"none"}
-          zIndex={100}
-        >
-          <IoIosArrowDropright size={"3rem"}></IoIosArrowDropright>
-        </Button>
-        {trailers?.results.map((trailer) => (
-          <SwiperSlide key={trailer.id}>
-            <video
-              className={styles.slide}
-              controls
-              poster={trailer.preview}
-              src={trailer.data[480]}
-              width="80%"
-            ></video>
-          </SwiperSlide>
-        ))}
-        {screenshots?.results.map((screenshot) => (
-          <SwiperSlide key={screenshot.id}>
-            <Image
-              aspectRatio={"16 / 9"}
-              className={styles.slide}
-              src={screenshot.image}
-            ></Image>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <Box>
+    <>
+      <Box marginBottom={5} position={"relative"}>
         <Swiper
-          className={styles.thumbnailsContainer}
-          modules={[Thumbs, FreeMode, Navigation]}
-          // @ts-ignore
-          onSwiper={setThumbsSwiper}
-          slidesPerView={4}
-          spaceBetween={20}
-          watchSlidesProgress
+          onSlideChange={(swiper) => {
+            let videoElements = document.getElementsByTagName("video");
+            for (let i = 0; i < videoElements.length; i++) {
+              videoElements[i].pause();
+            }
+            setSwiperActiveIndex(swiper.activeIndex);
+          }}
+          loop={false}
+          spaceBetween={50}
           navigation={{
-            nextEl: ".nextButtonThumb",
-            prevEl: ".prevButtonThumb",
+            nextEl: ".nextButton",
+            prevEl: ".prevButton",
+          }}
+          keyboard={true}
+          slidesPerView={1}
+          modules={[Thumbs, FreeMode, Navigation, Keyboard]}
+          thumbs={{
+            //@ts-ignoreF
+            swiper:
+              thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
           }}
         >
           <Button
-            className="prevButtonThumb"
-            left={"10px"}
-            onClick={() => {
-              if (thumbsSwiper) {
-                //@ts-ignore
-                thumbsSwiper.slidePrev();
-              }
-            }}
+            className="prevButton"
+            left={"12%"}
+            onClick={() => swiper.slidePrev()}
             padding={0}
             position={"absolute"}
             top={"50%"}
@@ -137,60 +111,128 @@ export const GameCarousel = ({ gameId }: Props) => {
             variant={"none"}
             zIndex={100}
           >
-            <IoIosArrowBack size={"2rem"}></IoIosArrowBack>
+            <IoIosArrowDropleft size={"3rem"}></IoIosArrowDropleft>
           </Button>
           <Button
-            className="nextButtonThumb"
-            onClick={() => {
-              if (thumbsSwiper) {
-                //@ts-ignore
-                thumbsSwiper.slideNext();
-              }
-            }}
+            className="nextButton"
+            onClick={() => swiper.slideNext()}
             padding={0}
             position={"absolute"}
-            right={"10px"}
+            right={"12%"}
             top={"50%"}
             transform={"translateY(-50%);"}
             variant={"none"}
             zIndex={100}
           >
-            <IoIosArrowForward size={"2rem"}></IoIosArrowForward>
+            <IoIosArrowDropright size={"3rem"}></IoIosArrowDropright>
           </Button>
-          {trailers?.results.map((trailer, i) => (
-            <SwiperSlide className={styles.videoThumbnail} key={trailer.id}>
-              <Image
-                aspectRatio={"16 / 9"}
-                className={
-                  swiperActiveIndex === i
-                    ? styles.activeSlide
-                    : styles.thumbnail
-                }
-                src={trailer.preview}
-                fallbackSrc={getCroppedImages(trailer.preview)}
-              ></Image>
-              <FaRegCirclePlay
-                size={40}
-                className={styles.videoPlay}
-              ></FaRegCirclePlay>
+          {trailers?.results.map((trailer) => (
+            <SwiperSlide key={trailer.id}>
+              <video
+                className={styles.slide}
+                controls
+                poster={trailer.preview}
+                src={trailer.data[480]}
+                width="80%"
+              ></video>
             </SwiperSlide>
           ))}
-          {screenshots?.results.map((screenshot, i) => (
+          {screenshots?.results.map((screenshot) => (
             <SwiperSlide key={screenshot.id}>
               <Image
                 aspectRatio={"16 / 9"}
-                className={
-                  swiperActiveIndex === i + (trailers?.count || 0)
-                    ? styles.activeSlide
-                    : styles.thumbnail
-                }
-                src={getCroppedImages(screenshot.image)}
+                className={styles.slide}
+                src={screenshot.image}
               ></Image>
             </SwiperSlide>
           ))}
         </Swiper>
+
+        <Box>
+          <Swiper
+            className={styles.thumbnailsContainer}
+            modules={[Thumbs, FreeMode, Navigation]}
+            // @ts-ignore
+            onSwiper={setThumbsSwiper}
+            slidesPerView={4}
+            spaceBetween={20}
+            watchSlidesProgress
+            navigation={{
+              nextEl: ".nextButtonThumb",
+              prevEl: ".prevButtonThumb",
+            }}
+          >
+            <Button
+              className="prevButtonThumb"
+              left={"10px"}
+              onClick={() => {
+                if (thumbsSwiper) {
+                  //@ts-ignore
+                  thumbsSwiper.slidePrev();
+                }
+              }}
+              padding={0}
+              position={"absolute"}
+              top={"50%"}
+              transform={"translateY(-50%);"}
+              variant={"none"}
+              zIndex={100}
+            >
+              <IoIosArrowBack size={"2rem"}></IoIosArrowBack>
+            </Button>
+            <Button
+              className="nextButtonThumb"
+              onClick={() => {
+                if (thumbsSwiper) {
+                  //@ts-ignore
+                  thumbsSwiper.slideNext();
+                }
+              }}
+              padding={0}
+              position={"absolute"}
+              right={"10px"}
+              top={"50%"}
+              transform={"translateY(-50%);"}
+              variant={"none"}
+              zIndex={100}
+            >
+              <IoIosArrowForward size={"2rem"}></IoIosArrowForward>
+            </Button>
+            {trailers?.results.map((trailer, i) => (
+              <SwiperSlide className={styles.videoThumbnail} key={trailer.id}>
+                <Image
+                  aspectRatio={"16 / 9"}
+                  className={
+                    swiperActiveIndex === i
+                      ? styles.activeSlide
+                      : styles.thumbnail
+                  }
+                  src={trailer.preview}
+                  fallbackSrc={getCroppedImages(trailer.preview)}
+                ></Image>
+                <FaRegCirclePlay
+                  size={40}
+                  className={styles.videoPlay}
+                ></FaRegCirclePlay>
+              </SwiperSlide>
+            ))}
+            {screenshots?.results.map((screenshot, i) => (
+              <SwiperSlide key={screenshot.id}>
+                <Image
+                  aspectRatio={"16 / 9"}
+                  className={
+                    swiperActiveIndex === i + (trailers?.count || 0)
+                      ? styles.activeSlide
+                      : styles.thumbnail
+                  }
+                  src={getCroppedImages(screenshot.image)}
+                ></Image>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
